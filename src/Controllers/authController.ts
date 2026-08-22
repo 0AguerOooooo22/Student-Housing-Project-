6364632321323d576a255252d0be5a51b83d42a3
youssef wael <207546198+youssefwael1010@users.noreply.github.com>
2026-08-22 13:51:51 +0300

    Remove token from user registration response
    Removed token from the response after user registration.

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -26,7 +26,6 @@ export const register = async (req: Request,res: Response): Promise<void> => {
 
         res.status(201).json({
             message: "User registered successfully.",
-            token,
             user: {
                 id: user._id,
                 fullName: user.fullName,
